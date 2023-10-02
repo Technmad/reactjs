66482e95a75686cebe233aa61be5cb8c0d8e0bb2
anurag <anuragpathakoffl@gmail.com>
2023-10-02 23:20:37 +0530

    updated main

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Greeting from "./greet.jsx";
+import GetList from "./list.jsx";
+import Bio from "./bio.jsx";
+import Avatar from "./pic.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <App />
+    <Greeting />
+    <GetList />
+    <Bio />
+    <Avatar />
   </React.StrictMode>
 );
