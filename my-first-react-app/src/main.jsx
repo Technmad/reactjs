8ba7984b6225d41c5efa82cc812b58a61adcd811
anurag <anuragpathakoffl@gmail.com>
2023-10-03 01:04:35 +0530

    added Fruit component

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Greeting from "./greet.jsx";
-import GetList from "./list.jsx";
-import Bio from "./bio.jsx";
-import Avatar from "./pic.jsx";
+import Fruits from "./rendering.jsx";
+// import Greeting from "./greet.jsx";
+// import GetList from "./list.jsx";
+// import Bio from "./bio.jsx";
+// import Avatar from "./pic.jsx";
+//import HeroList from "./rendering.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <App />
-    <Greeting />
+    {/* <Greeting />
     <GetList />
-    <Bio />
-    <Avatar />
+    <Bio /> */}
+    {/* <Avatar /> */}
+    {/* <HeroList /> */}
+    <Fruits />
   </React.StrictMode>
 );
