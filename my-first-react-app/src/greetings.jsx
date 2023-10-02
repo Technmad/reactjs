d34ebdd9e611bd8a72b0283c3488478dc0ad25ee
anurag <anuragpathakoffl@gmail.com>
2023-10-02 23:22:04 +0530

    react component

--- my-first-react-app/src/greetings.jsx
+++ my-first-react-app/src/greetings.jsx
@@ -1,10 +1,12 @@
 const GreetUser = () => {
+  const user = "Anurag";
   return (
     <>
-      <h1>welcome to TOP</h1>
-      <h3>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h3>
+      <h1 className="greetings">{user}, welcome to TOP</h1>
     </>
   );
 };
 
 export default GreetUser;
+
+// using JSX syntactical sugar instead of createElement()
