ed90e14d711a43d03142af36357c4176734383f1
anurag <anuragpathakoffl@gmail.com>
2023-11-14 14:26:43 +0530

    fixin jsx

--- 01app/src/App.jsx
+++ 01app/src/App.jsx
@@ -1,9 +1,12 @@
-import "./App.css";
+import Name from "./Name";
+import Items from "./Items";
 
 function App() {
   return (
     <>
       <h1>Namaste React!</h1>
+      <Name />
+      <Items />
     </>
   );
 }
---
+++ 01app/src/Name.jsx
@@ -0,0 +1,14 @@
+const Name = () => {
+  return (
+    <>
+      <div>Name</div>
+      <ul>
+        <li>Khushi</li>
+        <li>Jyoti</li>
+        <li>Pritika</li>
+      </ul>
+    </>
+  );
+};
+
+export default Name;
