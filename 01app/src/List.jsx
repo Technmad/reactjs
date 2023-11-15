69edaa11d9ec09d831872bb6142bb656156b60ab
anurag <anuragpathakoffl@gmail.com>
2023-11-15 23:53:45 +0530

    conditionals check

--- 01app/src/List.jsx
+++ 01app/src/List.jsx
@@ -1,9 +1,19 @@
 /* eslint-disable react/prop-types */
 function ListItems(props) {
+  if (!props.animal) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animal.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
   return (
     <ul>
       {props.animal.map((animal) => {
-        return animal.startsWith("L") ? <li key={animal}>{animal}</li> : null;
+        // return animal.startsWith("L") ? <li key={animal}>{animal}</li> : null;
+        // using && operator
+        return animal.startsWith("L") && <li key={animal}>{animal}</li>;
       })}
     </ul>
   );
