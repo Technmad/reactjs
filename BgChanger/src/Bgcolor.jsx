e408329096e59207a02073159739d63e27f882a2
anurag <anuragpathakoffl@gmail.com>
2023-12-10 16:55:35 +0530

    added bg color changer function

---
+++ BgChanger/src/Bgcolor.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const colors = ["red", "blue", "green"];
+
+function Bgcolor() {
+  const [color, setColor] = useState(colors[0]);
+
+  return (
+    <>
+      <div className="App" style={{ color }}>
+        {colors.map((color) => {
+          <button type="button" key={color} onClick={setColor}>
+            {color}
+          </button>;
+        })}
+      </div>
+    </>
+  );
+}
