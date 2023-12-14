614f523f5b27712531dc49d2d8bd552c9caecd36
anurag <anuragpathakoffl@gmail.com>
2023-12-15 00:06:35 +0530

    passing function as props

--- 01app/src/Buttons.jsx
+++ 01app/src/Buttons.jsx
@@ -1,17 +1,30 @@
 /* eslint-disable react/prop-types */
-function ButtonList({ text = "Click Me!", color = "blue", fontSize = 12 }) {
+function ButtonList({
+  text = "Click Me!",
+  color = "blue",
+  fontSize = 12,
+  handleClick,
+}) {
   const buttonStyle = {
     color: color,
     fontSize: fontSize + "px",
   };
 
-  return <button style={buttonStyle}>{text}</button>;
+  return (
+    <button onClick={handleClick} style={buttonStyle}>
+      {text}
+    </button>
+  );
 }
 
 const Buttons = () => {
+  const handleButtonClick = () => {
+    window.location.href = "https://www.google.com";
+  };
+
   return (
     <div>
-      <ButtonList text="Click Me!" color="blue" fontSize={12} />
+      <ButtonList handleClick={handleButtonClick} />
       <ButtonList text="Don't Click Me!" color="red" fontSize={15} />
       <ButtonList text="Click Me!" color="green" fontSize={20} />
     </div>
