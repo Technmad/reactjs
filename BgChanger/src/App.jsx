11772eb0b68c26b0ba05772e2fd405bad62ab5b7
anurag <anuragpathakoffl@gmail.com>
2023-12-24 13:17:06 +0530

    bg color changer

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+
+const colors = ["olive", "green", "blue", "yellow", "purple"];
+
 function App() {
-  return <></>;
+  const [backgroundColor, setBackgroundColor] = useState(colors[0]);
+
+  const onButtonClick = (color) => {
+    setBackgroundColor(color);
+  };
+
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor,
+      }}
+    >
+      {colors.map((color) => (
+        <button type="button" key={color} onClick={() => onButtonClick(color)}>
+          {color}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default App;
