5c7ba81567aaf1d79512962f215214ad4dc1dc54
anurag <anuragpathakoffl@gmail.com>
2023-12-24 13:28:21 +0530

    added counter

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -5,24 +5,33 @@ const colors = ["olive", "green", "blue", "yellow", "purple"];
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(colors[0]);
+  const [counter, setCounter] = useState(0);
 
   const onButtonClick = (color) => {
     setBackgroundColor(color);
+    setCounter(counter + 1);
   };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor,
-      }}
-    >
-      {colors.map((color) => (
-        <button type="button" key={color} onClick={() => onButtonClick(color)}>
-          {color}
-        </button>
-      ))}
-    </div>
+    <>
+      <div
+        className="App"
+        style={{
+          backgroundColor,
+        }}
+      >
+        {colors.map((color) => (
+          <button
+            type="button"
+            key={color}
+            onClick={() => onButtonClick(color)}
+          >
+            {color}
+          </button>
+        ))}
+        <div>Background Color changed {counter} times</div>
+      </div>
+    </>
   );
 }
 
